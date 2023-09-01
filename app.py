efda90796ddda657a4de91f57ea6ef9b8883811b
HSY <shinyeop1224@gmail.com>
2023-09-01 09:22:49 +0900

    update

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st
+import numpy as np
+
+def main():
+    
+    st.markdowm('# Hello World')
+    st.write(np.__vetsion__)
+
+if __name__ == '__main__':
+    main()
